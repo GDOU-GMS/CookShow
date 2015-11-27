e45622088b6c5fe5727c8ab11c7735308c7a734f
Mango <Mango@DESKTOP-SK1202O>
2015-11-27 16:30:07 +0800

    mango

--- src/main/java/com/blueshit/cookshow/service/impl/MenuServiceImpl.java
+++ src/main/java/com/blueshit/cookshow/service/impl/MenuServiceImpl.java
@@ -1,8 +1,12 @@
 package com.blueshit.cookshow.service.impl;
 
+import java.util.List;
+
+import com.blueshit.cookshow.common.utils.Configuration;
 import com.blueshit.cookshow.dao.impl.DaoSupportImpl;
 import com.blueshit.cookshow.model.entity.Menu;
 import com.blueshit.cookshow.service.MenuService;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,4 +16,15 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional
 public class MenuServiceImpl extends DaoSupportImpl<Menu> implements MenuService {
+
+
+//       public void getRecentpopularMenu(Menu menu){
+//          
+//       
+//       }
+//       
+       
+	
 }
+
+
