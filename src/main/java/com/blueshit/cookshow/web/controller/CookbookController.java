fc9b102a2012553a6a3a771fb56cd3b669815a4a
suhaixiasu <suhaixia98@163.com>
2015-12-01 16:18:21 +0800

    sss

--- src/main/java/com/blueshit/cookshow/web/controller/CookbookController.java
+++ src/main/java/com/blueshit/cookshow/web/controller/CookbookController.java
@@ -1,9 +1,13 @@
 package com.blueshit.cookshow.web.controller;
 
+import com.blueshit.cookshow.common.helper.Page;
+import com.blueshit.cookshow.common.helper.QueryHelper;
 import com.blueshit.cookshow.model.entity.Cookbook;
+import com.blueshit.cookshow.model.entity.User;
 import com.blueshit.cookshow.web.basic.BaseController;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,6 +17,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/cookbook")
 public class CookbookController extends BaseController {
+	
+	@RequestMapping("/list")
+	public String list(Integer pageNum,Model model){
+		
+		pageNum = pageNum==null||pageNum==0?1:pageNum;
+
+        QueryHelper queryHelper = new QueryHelper(Cookbook.class,"cb").addOrderByProperty("createDate",true);
+
+        Page page = cookbookService.getPage(pageNum, queryHelper);
+
+        model.addAttribute("page",page);
+
+        return "customer/menu/cookmenu";
+
+		
+		
+	
+	}
 	
 	@RequestMapping("/addCookbook")
 	public String addCookbook(@ModelAttribute Cookbook cookbook){
