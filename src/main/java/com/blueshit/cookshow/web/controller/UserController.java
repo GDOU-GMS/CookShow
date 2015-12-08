8883ed2f5b48eef0466e16a3b298b0c87e3e6058
suhaixiasu <suhaixia98@163.com>
2015-12-08 10:12:16 +0800

    sweet

--- src/main/java/com/blueshit/cookshow/web/controller/UserController.java
+++ src/main/java/com/blueshit/cookshow/web/controller/UserController.java
@@ -4,11 +4,13 @@ import com.blueshit.cookshow.common.helper.Page;
 import com.blueshit.cookshow.common.mail.MailUtils;
 import com.blueshit.cookshow.common.utils.MyDataUtils;
 import com.blueshit.cookshow.common.utils.UUIDCreator;
+import com.blueshit.cookshow.model.entity.Relation;
 import com.blueshit.cookshow.model.entity.User;
 import com.blueshit.cookshow.qiniu.QiniuUpload;
 import com.blueshit.cookshow.shiro.ShiroMD5;
 import com.blueshit.cookshow.web.basic.BaseController;
 import com.blueshit.cookshow.web.controller.common.ResultEntity;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -23,6 +25,7 @@ import javax.persistence.Entity;
 import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+
 import java.util.Date;
 import java.util.Iterator;
 
@@ -248,7 +251,8 @@ public class UserController extends BaseController {
                              String target,
                              Integer cookbookpageNum,
                              Integer menupageNum,
-                             Integer productionpageNum){
+                             Integer productionpageNum,
+                             HttpServletRequest request){
         cookbookpageNum = cookbookpageNum==null||cookbookpageNum<=0?1:cookbookpageNum;
         menupageNum = menupageNum==null||menupageNum<=0?1:menupageNum;
         productionpageNum = productionpageNum==null||productionpageNum<=0?1:productionpageNum;
@@ -269,6 +273,15 @@ public class UserController extends BaseController {
             //查询作品
             Page productionPage = productionService.getProductionByUserId(user.getId(),productionpageNum,20);
             model.addAttribute("productionPage",productionPage);
+            //查询是否已关注
+            User currentUser=(User) request.getSession().getAttribute("user");
+            if(currentUser!=null){
+            	  if(currentUser.getId()!=Long.parseLong(userId)){
+                   	model.addAttribute("tag", 1);
+            	  }
+	            Relation relation=relationService.getFocusOnFriend(currentUser.getId(), user.getId());
+	            model.addAttribute("relation",relation);
+            }
         }else{
             return "redirect:/error_404";
         }
